061c72f4bd0185289ff0d9afa4196b0440590c43
valdir.santos <valdir.santos@ifba.edu.br>
2021-08-08 21:52:23 -0300

    Projeto de Estudo no conda e jupyter notebook

--- escola/forms.py
+++ escola/forms.py
@@ -4,5 +4,12 @@ from .models import Professor
 class ProfessorForm(forms.Form):
     
     nome = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Digite o seu nome'}))
+    rg =forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Somente números'}))
+    data_nascimento = forms.DateField(widget=forms.DateInput(attrs={'placeholder':'Data de Nascimento'}))
+    endereco = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Rua/Bairro'}))
+    cep = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'CEP'}))
+    cidade = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Cidade'}))
+    telefone = forms.CharField(widget=forms.NumberInput(attrs={'placeholder':'Telefone/Celular'}))
+    #data_registro = models.CharField('Data de registro')
     ##model = Professor
-    fields = ['nome']
+    fields = ['__all__']
--- escola/models.py
+++ escola/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 class Professor(models.Model):
-    nome = models.CharField('Nome Professor', max_length=100)
+    nome = models.CharField('Nome Professor', max_length=100, blank=True)
+    rg = models.CharField('Identificação', max_length=12, blank=True)
+    data_nascimento = models.DateField('Data de Nascimento', null=True,blank=True)
+    endereco = models.CharField('Rua/Bairro', max_length=250, blank=True)
+    cep = models.CharField('CEP', max_length=10, blank=True)
+    cidade = models.CharField('Cidade',max_length=60,blank=True)
+    telefone = models.CharField('Telefone/Celular', max_length=15, blank=True)
+    data_registro = models.DateField(auto_now=True)
     
     def __str__(self):
         return self.nome
